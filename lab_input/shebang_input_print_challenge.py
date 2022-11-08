16ab7de2732250e1c4ac75dfe0977f2c9dd6fd60
MarkACodes <mark.stokes@tlgcohort.com>
2022-11-08 16:46:30 +0000

    variable and user input challenge added

---
+++ lab_input/shebang_input_print_challenge.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+name=input("What is your name?")
+print(name)
+
+dayOfTheWeek=input("What is the day of the week?")
+print(dayOfTheWeek)
+
+hello=("Hello " + name + " Happy " + dayOfTheWeek + " ! ")
+print(hello)
+
+
