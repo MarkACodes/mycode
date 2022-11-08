c3d7f1028d5c22eb1e9bdd189a9abb709ff6b6d3
MarkACodes <mark.stokes@tlgcohort.com>
2022-11-08 17:11:14 +0000

    better user input and separations

--- lab_input/shebang_input_print_challenge.py
+++ lab_input/shebang_input_print_challenge.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-name=input("What is your name?")
-print(name)
 
-dayOfTheWeek=input("What is the day of the week?")
-print(dayOfTheWeek)
+name=input("What is your name? ")
+print("Your name is: " + name)
 
-hello=("Hello " + name + " Happy " + dayOfTheWeek + " ! ")
+dayOfTheWeek=input("What is the day of the week? ")
+print("Today's day is: " + dayOfTheWeek)
+
+hello=("Hello, " + name + " Happy " + dayOfTheWeek + " ! ")
 print(hello)
+
+#TODO: further research on concat + versus comma , seperations
 
 
